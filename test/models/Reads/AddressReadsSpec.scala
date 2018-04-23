01c8e946ffa609d654c8eae21438569a39082633
juan <juanan_rp@hotmail.co.uk>
2018-04-23 08:44:17 +0100

    refactoring specs

--- test/models/Reads/AddressReadsSpec.scala
+++ test/models/Reads/AddressReadsSpec.scala
@@ -16,58 +16,62 @@
 
 package models.Reads
 
-import java.io
-
-import models.{ForeignAddress, UkAddress}
+import models.{Address, ForeignAddress, UkAddress}
 import org.scalatest.{MustMatchers, OptionValues, WordSpec}
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
 
 class AddressReadsSpec extends WordSpec with MustMatchers with OptionValues {
   "A JSON Payload with an address" should {
     "Map correctly to an Address type" when {
-      "We have a UK address" when {
+      "We have common elements of an address for a UK and NON UK address" when {
         val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
           "country" -> JsObject(Map("name" -> JsString("GB"))),
           "postcode" -> JsString("Test"))
 
         "We have line 1 in the address" in {
-          val result = input.as[UkAddress](UkAddress.apiReads)
+          val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonAddressElementsReads)
 
-          result.addressLine1 mustBe "line1"
+          result._1 mustBe "line1"
         }
 
         "We have line 2 in the address" in {
-          val result = input.as[UkAddress](UkAddress.apiReads)
+          val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonAddressElementsReads)
 
-          result.addressLine2.value mustBe "line2"
+          result._2.value mustBe "line2"
         }
 
         "We have line 3 in the address" in {
           val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"),JsString("line3"))),
             "country" -> JsObject(Map("name" -> JsString("GB"))),
             "postcode" -> JsString("Test"))
-          val result = input.as[UkAddress](UkAddress.apiReads)
+          val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonAddressElementsReads)
 
-          result.addressLine3.value mustBe "line3"
+          result._3.value mustBe "line3"
         }
 
         "We have line 4 in the address" in {
           val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"),JsString("line3"),JsString("line4"))),
             "country" -> JsObject(Map("name" -> JsString("GB"))),
             "postcode" -> JsString("Test"))
-          val result = input.as[UkAddress](UkAddress.apiReads)
+          val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonAddressElementsReads)
 
-          result.addressLine3.value mustBe "line3"
+          result._4.value mustBe "line4"
         }
 
         "We have a countryCode" in {
-          val result = input.as[UkAddress](UkAddress.apiReads)
+          val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonAddressElementsReads)
 
-          result.countryCode mustBe "GB"
+          result._5 mustBe "GB"
         }
+      }
 
+
+      "We have a UK address" when {
         "We have a postCode" in {
+          val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
+            "country" -> JsObject(Map("name" -> JsString("GB"))),
+            "postcode" -> JsString("Test"))
+
           val result = input.as[UkAddress](UkAddress.apiReads)
 
           result.postalCode mustBe "Test"
@@ -75,50 +79,20 @@ class AddressReadsSpec extends WordSpec with MustMatchers with OptionValues {
       }
 
       "We have a non UK address" when {
-        val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
-          "country" -> JsObject(Map("name" -> JsString("IT"))),
-          "postcode" -> JsString("Test"))
-
-        "We have line 1 in the address" in {
-          val result = input.as[ForeignAddress](ForeignAddress.apiReads)
-
-          result.addressLine1 mustBe "line1"
-        }
-
-        "We have line 2 in the address" in {
-          val result = input.as[ForeignAddress](ForeignAddress.apiReads)
-
-          result.addressLine2.value mustBe "line2"
-        }
-
-        "We have line 3 in the address" in {
-          val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"),JsString("line3"))),
-            "country" -> JsObject(Map("name" -> JsString("IT"))),
-            "postcode" -> JsString("Test"))
-          val result = input.as[ForeignAddress](ForeignAddress.apiReads)
-
-          result.addressLine3.value mustBe "line3"
-        }
-
-        "We have line 4 in the address" in {
-          val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"),JsString("line3"),JsString("line4"))),
-            "country" -> JsObject(Map("name" -> JsString("IT"))),
-            "postcode" -> JsString("Test"))
-          val result = input.as[ForeignAddress](ForeignAddress.apiReads)
-
-          result.addressLine3.value mustBe "line3"
-        }
-
-        "We have a countryCode" in {
-          val result = input.as[ForeignAddress](ForeignAddress.apiReads)
-
-          result.countryCode mustBe "IT"
-        }
+        val address = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
+          "country" -> JsObject(Map("name" -> JsString("IT"))))
 
         "We have a postCode" in {
-          val result = input.as[ForeignAddress](ForeignAddress.apiReads)
+          val result = (address + ("postcode" -> JsString("Test"))).as[ForeignAddress](ForeignAddress.apiReads)
 
           result.postalCode.value mustBe "Test"
+        }
+
+
+        "We don't have a postCode" in {
+          val result = address.as[ForeignAddress](ForeignAddress.apiReads)
+
+          result.postalCode mustBe None
         }
       }
     }
