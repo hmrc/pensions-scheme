7525c69c73b9436dc0b3d44f44d614b7d98b26c4
juan <juanan_rp@hotmail.co.uk>
2018-04-25 08:50:30 +0100

    refactoring

--- app/models/Address.scala
+++ app/models/Address.scala
@@ -22,7 +22,6 @@ import play.api.libs.json._
 sealed trait Address
 
 object Address {
-
   /*{
       "lines" : [
           "test test",
@@ -35,12 +34,7 @@ object Address {
           "name" : "GB"
       }
     }*/
-  val addressTypeOneReads: Reads[Address] = (__ \ "country" \ "name").read[String].flatMap {
-    case "GB" =>
-      UkAddress.apiAddressTypeOneReads.map(c=>c.asInstanceOf[Address])
-    case _ =>
-      ForeignAddress.apiAddressTypeOneReads.map(c=>c.asInstanceOf[Address])
-  }
+  val addressTypeOneReads: Reads[Address] = (__ \ "country" \ "name").read[String].flatMap(c => getRightTypeOfReadsBasedOnCountry[UkAddress, ForeignAddress](UkAddress.apiAddressTypeOneReads, ForeignAddress.apiAddressTypeOneReads, c))
 
   /* {
         "addressLine1" : "100 SuttonStreet",
@@ -50,12 +44,7 @@ object Address {
         "postalCode" : "DH14EJ",
         "countryCode" : "GB"
     }*/
-  val addressTypeTwoReads : Reads[Address] = (__ \ "countryCode").read[String].flatMap {
-    case "GB" =>
-      UkAddress.apiAddressTypeTwoReads.map(c=>c.asInstanceOf[Address])
-    case _ =>
-      ForeignAddress.apiAddressTypeTwoReads.map(c=>c.asInstanceOf[Address])
-  }
+  val addressTypeTwoReads: Reads[Address] = (__ \ "countryCode").read[String].flatMap(c => getRightTypeOfReadsBasedOnCountry[UkAddress, ForeignAddress](UkAddress.apiAddressTypeTwoReads, ForeignAddress.apiAddressTypeTwoReads, c))
 
   implicit val reads: Reads[Address] = JsPath.read(addressTypeOneReads) | JsPath.read(addressTypeTwoReads)
 
@@ -66,26 +55,35 @@ object Address {
       ForeignAddress.format.writes(address)
   }
 
-  val commonTypeOneAddressElementsReads : Reads[(String,Option[String],Option[String],Option[String],String)] = (
+  val commonTypeOneAddressElementsReads: Reads[(String, Option[String], Option[String], Option[String], String)] = (
     (JsPath \ "lines").read[List[String]] and
       (JsPath \ "country" \ "name").read[String]
-    )((lines, countryCode) => {
+    ) ((lines, countryCode) => {
     val addressLines = lines.size match {
-      case 2 => List(Some(lines(0)),Some(lines(1)),None, None)
-      case 3 => List(Some(lines(0)),Some(lines(1)),Some(lines(2)), None)
-      case 4 => List(Some(lines(0)),Some(lines(1)),Some(lines(2)),Some(lines(3)))
+      case 2 => List(Some(lines(0)), Some(lines(1)), None, None)
+      case 3 => List(Some(lines(0)), Some(lines(1)), Some(lines(2)), None)
+      case 4 => List(Some(lines(0)), Some(lines(1)), Some(lines(2)), Some(lines(3)))
     }
-    (addressLines(0).get,addressLines(1),addressLines(2),addressLines(3),countryCode)
+    (addressLines(0).get, addressLines(1), addressLines(2), addressLines(3), countryCode)
   })
 
-  val commonTypeTwoAddressElementsReads : Reads[(String,Option[String],Option[String],Option[String],String)] = (
+  val commonTypeTwoAddressElementsReads: Reads[(String, Option[String], Option[String], Option[String], String)] = (
     (JsPath \ "addressLine1").read[String] and
       (JsPath \ "addressLine2").readNullable[String] and
       (JsPath \ "addressLine3").readNullable[String] and
       (JsPath \ "addressLine4").readNullable[String] and
       (JsPath \ "countryCode").read[String]
-    )((line1, line2, line3, line4, countryCode) => (line1,line2,line3,line4,countryCode))
+    ) ((line1, line2, line3, line4, countryCode) => (line1, line2, line3, line4, countryCode))
+
+  private def getRightTypeOfReadsBasedOnCountry[T, B](ukAddressReads: Reads[T], nonUkAddressReads: Reads[B], test: String) = {
+    test match {
+      case "GB" =>
+        ukAddressReads.map(c => c.asInstanceOf[Address])
+      case _ =>
+        nonUkAddressReads.map(c => c.asInstanceOf[Address])
+    }
   }
+}
 
 case class UkAddress(addressLine1: String, addressLine2: Option[String] = None, addressLine3: Option[String] = None,
                      addressLine4: Option[String] = None, countryCode: String, postalCode: String) extends Address
@@ -98,17 +96,17 @@ object UkAddress {
       Json.writes[UkAddress].writes(address) ++ Json.obj("countryCode" -> "GB")
   }
 
-  val apiAddressTypeOneReads : Reads[UkAddress] = (
+  val apiAddressTypeOneReads: Reads[UkAddress] = (
     JsPath.read(Address.commonTypeOneAddressElementsReads) and
       (JsPath \ "postcode").read[String]
-    )((common, postCode) =>  {
-    UkAddress(common._1,common._2,common._3,common._4,common._5, postCode)
+    ) ((common, postCode) => {
+    UkAddress(common._1, common._2, common._3, common._4, common._5, postCode)
   })
 
-  val apiAddressTypeTwoReads : Reads[UkAddress] = (
+  val apiAddressTypeTwoReads: Reads[UkAddress] = (
     JsPath.read(Address.commonTypeTwoAddressElementsReads) and
       (JsPath \ "postalCode").read[String]
-    )((common,postalCode)=>UkAddress(common._1,common._2,common._3,common._4,common._5,postalCode))
+    ) ((common, postalCode) => UkAddress(common._1, common._2, common._3, common._4, common._5, postalCode))
 }
 
 case class ForeignAddress(addressLine1: String, addressLine2: Option[String] = None, addressLine3: Option[String] = None,
@@ -117,17 +115,17 @@ case class ForeignAddress(addressLine1: String, addressLine2: Option[String] = N
 object ForeignAddress {
   implicit val format: Format[ForeignAddress] = Json.format[ForeignAddress]
 
-  val apiAddressTypeTwoReads : Reads[ForeignAddress] = (
+  val apiAddressTypeTwoReads: Reads[ForeignAddress] = (
     JsPath.read(Address.commonTypeTwoAddressElementsReads) and
       (JsPath \ "postalCode").readNullable[String]
-    )((common,postalCode)=>ForeignAddress(common._1,common._2,common._3,common._4,common._5,postalCode))
+    ) ((common, postalCode) => ForeignAddress(common._1, common._2, common._3, common._4, common._5, postalCode))
 
 
-  val apiAddressTypeOneReads : Reads[ForeignAddress] = (
+  val apiAddressTypeOneReads: Reads[ForeignAddress] = (
     JsPath.read(Address.commonTypeOneAddressElementsReads) and
       (JsPath \ "postcode").readNullable[String]
-    )((common, postCode) =>  {
-    ForeignAddress(common._1,common._2,common._3,common._4,common._5, postCode)
+    ) ((common, postCode) => {
+    ForeignAddress(common._1, common._2, common._3, common._4, common._5, postCode)
   })
 }
 
