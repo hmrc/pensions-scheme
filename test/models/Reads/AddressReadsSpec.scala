953bf226adb9fa584b00b3d34d13f927ae8582bf
juan <juanan_rp@hotmail.co.uk>
2018-04-25 11:51:04 +0100

    refactoring address reads spec and adding clarification to what type on and type 2 of address is

--- test/models/Reads/AddressReadsSpec.scala
+++ test/models/Reads/AddressReadsSpec.scala
@@ -23,80 +23,102 @@ import play.api.libs.json._
 class AddressReadsSpec extends WordSpec with MustMatchers with OptionValues with Samples {
   "A JSON Payload with an address" should {
     "Map correctly to an Address type" when {
-      "We have common elements of an address for a UK and NON UK address" when {
-        val address = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
-          "country" -> JsObject(Map("name" -> JsString("GB"))),
-          "postcode" -> JsString("NE1"))
-
-        "We have line 1 in the address" in {
-          val result = address.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
-
-          result._1 mustBe ukAddressSample.addressLine1
-        }
-
-        "We have line 2 in the address" in {
-          val result = address.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
-
-          result._2 mustBe ukAddressSample.addressLine2
-        }
-
-        "We have line 3 in the address" in {
-          val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"),JsString("line3"))),
-            "country" -> JsObject(Map("name" -> JsString("GB"))),
-            "postcode" -> JsString("Test"))
-          val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
-
-          result._3 mustBe ukAddressSample.addressLine3
-        }
-
-        "We have line 4 in the address" in {
-          val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"),JsString("line3"),JsString("line4"))),
-            "country" -> JsObject(Map("name" -> JsString("GB"))),
-            "postcode" -> JsString("Test"))
-          val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
-
-          result._4 mustBe ukAddressSample.addressLine4
-        }
-
-        "We have a countryCode" in {
-          val result = address.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
-
-          result._5 mustBe ukAddressSample.countryCode
-        }
-      }
-
-
-      "We have a UK address" when {
-        "We have a postCode" in {
-          val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
+      "We have an address in format type one" when {
+        /*{
+          "lines" : [
+              "test test",
+              "test 2"
+          ],
+          "town" : "test 2",
+          "county" : "test 2",
+          "postcode" : "NE21 2LG",
+          "country" : {
+              "name" : "GB"
+          }
+        }*/
+        "We have common elements of an address for a UK and NON UK address" when {
+          val address = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
             "country" -> JsObject(Map("name" -> JsString("GB"))),
             "postcode" -> JsString("NE1"))
 
-          val result = input.as[Address]
+          "We have line 1 in the address" in {
+            val result = address.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
 
-          result.asInstanceOf[UkAddress].postalCode mustBe ukAddressSample.postalCode
+            result._1 mustBe ukAddressSample.addressLine1
+          }
+
+          "We have line 2 in the address" in {
+            val result = address.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
+
+            result._2 mustBe ukAddressSample.addressLine2
+          }
+
+          "We have line 3 in the address" in {
+            val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"),JsString("line3"))),
+              "country" -> JsObject(Map("name" -> JsString("GB"))),
+              "postcode" -> JsString("Test"))
+            val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
+
+            result._3 mustBe ukAddressSample.addressLine3
+          }
+
+          "We have line 4 in the address" in {
+            val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"),JsString("line3"),JsString("line4"))),
+              "country" -> JsObject(Map("name" -> JsString("GB"))),
+              "postcode" -> JsString("Test"))
+            val result = input.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
+
+            result._4 mustBe ukAddressSample.addressLine4
+          }
+
+          "We have a countryCode" in {
+            val result = address.as[(String,Option[String],Option[String],Option[String],String)](Address.commonTypeOneAddressElementsReads)
+
+            result._5 mustBe ukAddressSample.countryCode
+          }
+        }
+
+
+        "We have a UK address" when {
+          "We have a postCode" in {
+            val input = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
+              "country" -> JsObject(Map("name" -> JsString("GB"))),
+              "postcode" -> JsString("NE1"))
+
+            val result = input.as[Address]
+
+            result.asInstanceOf[UkAddress].postalCode mustBe ukAddressSample.postalCode
+          }
+        }
+
+        "We have a non UK address" when {
+          val address = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
+            "country" -> JsObject(Map("name" -> JsString("IT"))))
+
+          "We have a postCode" in {
+            val result = (address + ("postcode" -> JsString("NE1"))).as[Address]
+
+            result.asInstanceOf[InternationalAddress].postalCode mustBe nonUkAddressSample.postalCode
+          }
+
+
+          "We don't have a postCode" in {
+            val result = address.as[Address]
+
+            result.asInstanceOf[InternationalAddress].postalCode mustBe None
+          }
         }
       }
 
-      "We have a non UK address" when {
-        val address = Json.obj("lines" -> JsArray(Seq(JsString("line1"),JsString("line2"))),
-          "country" -> JsObject(Map("name" -> JsString("IT"))))
-
-        "We have a postCode" in {
-          val result = (address + ("postcode" -> JsString("NE1"))).as[Address]
-
-          result.asInstanceOf[InternationalAddress].postalCode mustBe nonUkAddressSample.postalCode
-        }
-
-
-        "We don't have a postCode" in {
-          val result = address.as[Address]
-
-          result.asInstanceOf[InternationalAddress].postalCode mustBe None
-        }
-      }
-
-      "We have a different address format" when {
+      "We have a different address in format type 2" when {
+        /*{
+          "addressLine1" : "100 SuttonStreet",
+          "addressLine2" : "Wokingham",
+          "addressLine3" : "Surrey",
+          "addressLine4" : "London",
+          "postalCode" : "DH14EJ",
+          "countryCode" : "GB"
+        }*/
         "we have common address elements" when {
           val address = Json.obj("addressLine1" -> JsString("line1"), "addressLine2" -> JsString("line2"), "addressLine3" -> JsString("line3"), "addressLine4" -> JsString("line4"),
             "postalCode" -> JsString("NE1"), "countryCode" -> JsString("GB"))
