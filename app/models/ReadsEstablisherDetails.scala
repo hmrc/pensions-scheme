47d300bc7a0a5db95c065d8ae0cbcd451ac50fc8
Paul Durham <pcdurham@googlemail.com>
2018-05-03 10:45:44 +0100

    PODS-844: JSON reads for EstablisherDetails
    Tidy up
    
    Directors have no organisation name
    
    Fix afer rebase

---
+++ app/models/ReadsEstablisherDetails.scala
@@ -0,0 +1,244 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.libs.json.{JsPath, Reads}
+import play.api.libs.functional.syntax._
+
+object ReadsEstablisherDetails {
+
+  private def previousAddressDetails(addressYears: String, previousAddress: Option[Address]): Option[PreviousAddressDetails] = {
+    if (addressYears == "under_a_year") {
+      Some(
+        PreviousAddressDetails(
+          isPreviousAddressLast12Month = true,
+          previousAddressDetails = previousAddress
+        )
+      )
+    }
+    else {
+      None
+    }
+  }
+
+  private implicit val readsContactDetails: Reads[ContactDetails] = (
+    (JsPath \ "emailAddress").read[String] and
+      (JsPath \ "phoneNumber").read[String]
+    )((email, phone) => ContactDetails(telephone = phone, email = email))
+
+  private implicit val readsAddress: Reads[Address] = (
+    (JsPath \ "addressLine1").read[String] and
+    (JsPath \ "addressLine2").read[String] and
+    (JsPath \ "addressLine3").readNullable[String] and
+    (JsPath \ "addressLine4").readNullable[String] and
+    (JsPath \ "country").read[String] and
+    (JsPath \ "postcode").readNullable[String]
+  )((addressLine1, addressLine2, addressLine3, addressLine4, countryCode, postalCode) => {
+    if (countryCode == "GB") {
+      postalCode match {
+        case Some(zip) =>
+          UkAddress(
+            addressLine1,
+            Some(addressLine2),
+            addressLine3,
+            addressLine4,
+            countryCode,
+            zip
+          )
+        case _ =>
+          throw new IllegalArgumentException("Null postcode in UK address")
+      }
+    }
+    else {
+      InternationalAddress(
+        addressLine1,
+        Some(addressLine2),
+        addressLine3,
+        addressLine4,
+        countryCode,
+        postalCode
+      )
+    }
+  })
+
+  private implicit val readsPersonalDetails: Reads[PersonalDetails] = (
+    (JsPath \ "firstName").read[String] and
+    (JsPath \ "middleName").readNullable[String] and
+    (JsPath \ "lastName").read[String] and
+    (JsPath \ "date").read[String]
+  )((firstName, middleName, lastName, dateOfBirth) =>
+    PersonalDetails(
+      None,
+      firstName,
+      middleName,
+      lastName,
+      dateOfBirth
+    )
+  )
+
+  private val readsEstablisherIndividual: Reads[EstablisherDetails] = (
+    (JsPath \ "establisherDetails").read[PersonalDetails] and
+    (JsPath \ "address").read[Address] and
+    (JsPath \ "contactDetails").read[ContactDetails] and
+    (JsPath \ "establisherNino" \ "nino").readNullable[String] and
+    (JsPath \ "establisherNino" \ "reason").readNullable[String] and
+    (JsPath \ "uniqueTaxReference" \ "utr").readNullable[String] and
+    (JsPath \ "uniqueTaxReference" \ "reason").readNullable[String] and
+    (JsPath \ "addressYears").read[String] and
+    (JsPath \ "previousAddress").readNullable[Address]
+  )((personalDetails, address, contactDetails, nino, noNinoReason, utr, noUtrReason, addressYears, previousAddress) =>
+    EstablisherDetails(
+      `type` = "Individual",
+      personalDetails = Some(personalDetails),
+      referenceOrNino = nino,
+      noNinoReason = noNinoReason,
+      utr = utr,
+      noUtrReason = noUtrReason,
+      correspondenceAddressDetails = CorrespondenceAddressDetails(address),
+      correspondenceContactDetails = CorrespondenceContactDetails(contactDetails),
+      previousAddressDetails = previousAddressDetails(addressYears, previousAddress)
+    )
+  )
+
+  private val readsCompanyDirector: Reads[EstablisherDetails] = (
+    (JsPath \ "directorDetails").read[PersonalDetails] and
+    (JsPath \ "directorAddressId").read[Address] and
+    (JsPath \ "directorContactDetails").read[ContactDetails] and
+    (JsPath \ "directorNino" \ "nino").readNullable[String] and
+    (JsPath \ "directorNino" \ "reason").readNullable[String] and
+    (JsPath \ "directorUniqueTaxReference" \ "utr").readNullable[String] and
+    (JsPath \ "directorUniqueTaxReference" \ "reason").readNullable[String] and
+    (JsPath \ "companyDirectorAddressYears").read[String] and
+    (JsPath \ "previousAddress").readNullable[Address]
+  )((personalDetails, address, contactDetails, nino, noNinoReason, utr, noUtrReason, addressYears, previousAddress) =>
+    EstablisherDetails(
+      `type` = "Director",
+      personalDetails = Some(personalDetails),
+      referenceOrNino = nino,
+      noNinoReason = noNinoReason,
+      utr = utr,
+      noUtrReason = noUtrReason,
+      correspondenceAddressDetails = CorrespondenceAddressDetails(address),
+      correspondenceContactDetails = CorrespondenceContactDetails(contactDetails),
+      previousAddressDetails = previousAddressDetails(addressYears, previousAddress)
+    )
+  )
+
+  private val readsCompanyDirectors: Reads[Seq[EstablisherDetails]] =
+    Reads.seq(readsCompanyDirector)
+
+  private val readsEstablisherCompany: Reads[Seq[EstablisherDetails]] = (
+    (JsPath \ "companyDetails" \ "companyName").read[String] and
+    (JsPath \ "companyDetails" \ "vatNumber").readNullable[String] and
+    (JsPath \ "companyDetails" \ "payeNumber").readNullable[String] and
+    (JsPath \ "companyUniqueTaxReference" \ "utr").readNullable[String] and
+    (JsPath \ "companyUniqueTaxReference" \ "reason").readNullable[String] and
+    (JsPath \ "companyRegistrationNumber" \ "crn").readNullable[String] and
+    (JsPath \ "companyRegistrationNumber" \ "reason").readNullable[String] and
+    (JsPath \ "otherDirectors").readNullable[Boolean] and
+    (JsPath \ "companyAddress").read[Address] and
+    (JsPath \ "companyContactDetails").read[ContactDetails] and
+    (JsPath \ "companyAddressYears").read[String] and
+    (JsPath \ "companyPreviousAddress").readNullable[Address] and
+    (JsPath \ "director").readNullable(readsCompanyDirectors)
+  )((companyName, vat, paye, utr, noUtrReason, crn, noCrnReason, otherDirectors, address, contactDetails, addressYears, previousAddress, directors) =>
+    EstablisherDetails(
+      `type` = "Company/Org",
+      organisationName = Some(companyName),
+      utr = utr,
+      noUtrReason = noUtrReason,
+      crnNumber = crn,
+      noCrnReason = noCrnReason,
+      vatRegistrationNumber = vat,
+      payeReference = paye,
+      haveMoreThanTenDirectorOrPartner = otherDirectors,
+      correspondenceAddressDetails = CorrespondenceAddressDetails(address),
+      correspondenceContactDetails = CorrespondenceContactDetails(contactDetails),
+      previousAddressDetails = previousAddressDetails(addressYears, previousAddress)
+    ) +: directors.getOrElse(Seq.empty[EstablisherDetails])
+  )
+
+  private val readsTrusteeCompany: Reads[EstablisherDetails] = (
+    (JsPath \ "companyDetails" \ "companyName").read[String] and
+    (JsPath \ "companyDetails" \ "vatNumber").readNullable[String] and
+    (JsPath \ "companyDetails" \ "payeNumber").readNullable[String] and
+    (JsPath \ "companyUniqueTaxReference" \ "utr").readNullable[String] and
+    (JsPath \ "companyUniqueTaxReference" \ "reason").readNullable[String] and
+    (JsPath \ "companyRegistrationNumber" \ "crn").readNullable[String] and
+    (JsPath \ "companyRegistrationNumber" \ "reason").readNullable[String] and
+    (JsPath \ "companyAddress").read[Address] and
+    (JsPath \ "companyContactDetails").read[ContactDetails] and
+    (JsPath \ "trusteesCompanyAddressYears").read[String] and
+    (JsPath \ "companyPreviousAddress").readNullable[Address]
+  )((companyName, vat, paye, utr, noUtrReason, crn, noCrnReason, address, contactDetails, addressYears, previousAddress) =>
+    EstablisherDetails(
+      `type` = "Company/Org Trustee",
+      organisationName = Some(companyName),
+      utr = utr,
+      noUtrReason = noUtrReason,
+      crnNumber = crn,
+      noCrnReason = noCrnReason,
+      vatRegistrationNumber = vat,
+      payeReference = paye,
+      correspondenceAddressDetails = CorrespondenceAddressDetails(address),
+      correspondenceContactDetails = CorrespondenceContactDetails(contactDetails),
+      previousAddressDetails = previousAddressDetails(addressYears, previousAddress)
+    )
+  )
+
+  private val readsTrusteeIndividual: Reads[EstablisherDetails] = (
+    (JsPath \ "trusteeDetails").read[PersonalDetails] and
+    (JsPath \ "trusteeAddressId").read[Address] and
+    (JsPath \ "trusteeContactDetails").read[ContactDetails] and
+    (JsPath \ "trusteeNino" \ "nino").readNullable[String] and
+    (JsPath \ "trusteeNino" \ "reason").readNullable[String] and
+    (JsPath \ "uniqueTaxReference" \ "utr").readNullable[String] and
+    (JsPath \ "uniqueTaxReference" \ "reason").readNullable[String] and
+    (JsPath \ "trusteeAddressYears").read[String] and
+    (JsPath \ "trusteePreviousAddress").readNullable[Address]
+  )((personalDetails, address, contactDetails, nino, noNinoReason, utr, noUtrReason, addressYears, previousAddress) =>
+    EstablisherDetails(
+      `type` = "Individual Trustee",
+      personalDetails = Some(personalDetails),
+      referenceOrNino = nino,
+      noNinoReason = noNinoReason,
+      utr = utr,
+      noUtrReason = noUtrReason,
+      correspondenceAddressDetails = CorrespondenceAddressDetails(address),
+      correspondenceContactDetails = CorrespondenceContactDetails(contactDetails),
+      previousAddressDetails = previousAddressDetails(addressYears, previousAddress)
+    )
+  )
+
+  private val readsEstablishers: Reads[Seq[EstablisherDetails]] =
+    Reads.seq(readsEstablisherCompany orElse readsEstablisherIndividual.map(i => Seq(i))).map(_.flatten)
+
+  private val readsTrustees: Reads[Seq[EstablisherDetails]] =
+    Reads.seq(readsTrusteeCompany orElse readsTrusteeIndividual)
+
+  implicit val readsEstablisherDetails: Reads[Seq[EstablisherDetails]] = (
+    (JsPath \ "establishers").readNullable(readsEstablishers) and
+    (JsPath \ "trustees").readNullable(readsTrustees)
+  )((
+      establisherCompanies,
+      trusteeCompanies
+    ) =>
+      establisherCompanies.getOrElse(Seq.empty[EstablisherDetails]) ++
+      trusteeCompanies.getOrElse(Seq.empty[EstablisherDetails])
+  )
+
+}
---
+++ test/models/Reads/establishers/EstablishersTestData.scala
@@ -0,0 +1,240 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.Reads.establishers
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import models._
+
+import scala.util.Random
+
+// scalastyle:off magic.number
+object EstablishersTestData {
+
+  private def alphanumeric(length: Int) =
+    Random.alphanumeric.take(length).mkString
+
+  private def numeric(length: Int) =
+    (1 to length).foldLeft("")((s, _) => s + Random.nextInt(10).toString)
+
+  private val dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+  private def date: String =
+    LocalDate.of(1950, 1, 1).plusDays(Random.nextInt(365 * 50)).format(dateFormatter)
+
+  private def testAddress = {
+    Random.nextInt(2) match {
+      case 0 =>
+        UkAddress(
+          Random.alphanumeric.take(35).mkString,
+          Some(alphanumeric(35)),
+          Some(alphanumeric(35)),
+          Some(alphanumeric(35)),
+          "GB",
+          alphanumeric(10)
+        )
+      case _ =>
+        InternationalAddress(
+          Random.alphanumeric.take(35).mkString,
+          Some(alphanumeric(35)),
+          Some(alphanumeric(35)),
+          Some(alphanumeric(35)),
+          "ES",
+          Some(alphanumeric(10))
+        )
+    }
+  }
+
+  private def testCompanyName =
+    alphanumeric(35)
+
+  private def testEmail =
+    alphanumeric(20) + "@test.net"
+
+  def testCompany(
+      companyType: CompanyType,
+      hasUtr: Boolean,
+      hasCrn: Boolean,
+      hasVat: Boolean,
+      hasPaye: Boolean,
+      otherDirectors: Option[Boolean],
+      hasPreviousAddress: Boolean): EstablisherDetails =
+
+    EstablisherDetails(
+      `type` = companyType.apiType,
+      organisationName = Some(testCompanyName),
+      utr = if (hasUtr) Some(alphanumeric(10)) else None,
+      noUtrReason = if (hasUtr) None else Some(alphanumeric(20)),
+      crnNumber = if (hasCrn) Some(alphanumeric(10)) else None,
+      noCrnReason = if (hasCrn) None else Some(alphanumeric(20)),
+      vatRegistrationNumber = if (hasVat) Some(numeric(9)) else None,
+      payeReference = if (hasPaye) Some(alphanumeric(13)) else None,
+      haveMoreThanTenDirectorOrPartner = otherDirectors,
+      correspondenceAddressDetails = CorrespondenceAddressDetails(testAddress),
+      correspondenceContactDetails = CorrespondenceContactDetails(
+        ContactDetails(
+          telephone = numeric(24),
+          email = testEmail
+        )
+      ),
+      previousAddressDetails =
+        if (hasPreviousAddress) {
+          Some(
+            PreviousAddressDetails(
+              isPreviousAddressLast12Month = true,
+              previousAddressDetails = Some(testAddress)
+            )
+          )
+        }
+        else {
+          None
+        }
+    )
+
+  def testIndividual(
+      individualType: IndividualType,
+      hasNino: Boolean = true,
+      hasUtr: Boolean = true,
+      hasPreviousAddress: Boolean = true): EstablisherDetails =
+
+    EstablisherDetails(
+      `type` = individualType.apiType,
+      personalDetails = Some(PersonalDetails(
+        firstName = alphanumeric(35),
+        middleName = Some(alphanumeric(35)),
+        lastName = alphanumeric(35),
+        dateOfBirth = date
+      )),
+      referenceOrNino = if (hasNino) Some(alphanumeric(10)) else None,
+      noNinoReason = if (hasNino) None else Some(alphanumeric(20)),
+      utr = if (hasUtr) Some(alphanumeric(10)) else None,
+      noUtrReason = if (hasUtr) None else Some(alphanumeric(20)),
+      correspondenceAddressDetails = CorrespondenceAddressDetails(testAddress),
+      correspondenceContactDetails = CorrespondenceContactDetails(
+        ContactDetails(
+          telephone = numeric(24),
+          email = testEmail
+        )
+      ),
+      previousAddressDetails =
+        if (hasPreviousAddress) {
+          Some(
+            PreviousAddressDetails(
+              isPreviousAddressLast12Month = true,
+              previousAddressDetails = Some(testAddress)
+            )
+          )
+        }
+        else {
+          None
+        }
+    )
+
+}
+
+case class CompanyBuilder(
+                           companyType: CompanyType,
+                           hasUtr: Boolean,
+                           hasCrn: Boolean,
+                           hasVat: Boolean,
+                           hasPaye: Boolean,
+                           otherDirectors: Option[Boolean],
+                           hasPreviousAddress: Boolean) {
+
+  import EstablishersTestData._
+
+  def withUtr(): CompanyBuilder =
+    copy(hasUtr = true)
+
+  def withCrn(): CompanyBuilder =
+    copy(hasCrn = true)
+
+  def withVat(): CompanyBuilder =
+    copy(hasVat = true)
+
+  def withPaye(): CompanyBuilder =
+    copy(hasPaye = true)
+
+  def withOtherDirectors(otherDirectors: Boolean): CompanyBuilder =
+    copy(otherDirectors = Some(otherDirectors))
+
+  def withPreviousAddress(): CompanyBuilder =
+    copy(hasPreviousAddress = true)
+
+  def build(): EstablisherDetails =
+    testCompany(
+      companyType,
+      hasUtr,
+      hasCrn,
+      hasVat,
+      hasPaye,
+      otherDirectors,
+      hasPreviousAddress
+    )
+
+}
+
+object CompanyBuilder {
+
+  def apply(companyType: CompanyType): CompanyBuilder =
+    CompanyBuilder(
+      companyType = companyType,
+      hasUtr = false,
+      hasCrn = false,
+      hasVat = false,
+      hasPaye = false,
+      otherDirectors = None,
+      hasPreviousAddress = false
+    )
+
+}
+
+case class IndividualBuilder(individualType: IndividualType, hasNino: Boolean, hasUtr: Boolean, hasPreviousAddress: Boolean) {
+
+  import EstablishersTestData._
+
+  def withNino(): IndividualBuilder =
+    copy(hasNino = true)
+
+  def withUtr(): IndividualBuilder =
+    copy(hasUtr = true)
+
+  def withPreviousAddress(): IndividualBuilder =
+    copy(hasPreviousAddress = true)
+
+  def build(): EstablisherDetails =
+    testIndividual(
+      individualType,
+      hasNino,
+      hasUtr,
+      hasPreviousAddress
+    )
+
+}
+
+object IndividualBuilder {
+
+  def apply(individualType: IndividualType): IndividualBuilder =
+    IndividualBuilder(
+      individualType = individualType,
+      hasNino = false,
+      hasUtr = false,
+      hasPreviousAddress = false
+    )
+
+}
