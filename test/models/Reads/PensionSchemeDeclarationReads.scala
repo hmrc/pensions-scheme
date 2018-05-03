5f078e21da6e660e12792023da81283d42a719c9
Andrew Arkless <AndrewArkless@users.noreply.github.com>
2018-05-03 16:47:18 +0100

    PODS-843 refactor of tests

--- test/models/Reads/PensionSchemeDeclarationReads.scala
+++ test/models/Reads/PensionSchemeDeclarationReads.scala
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 package models.Reads
 
 import models._
@@ -36,7 +35,7 @@ class PensionSchemeDeclarationReads extends WordSpec with MustMatchers with Opti
          result.box9 mustBe true
        }
 
-       "It is true then boxes 1,2,6 7,8 and 9 are false" in {
+       "It is false then boxes 1,2,6 7,8 and 9 are false" in {
          val result = (declaration + ("declaration" -> JsBoolean(false))).as[PensionSchemeDeclaration](PensionSchemeDeclaration.apiReads)
          result.box1 mustBe false
          result.box2 mustBe false
@@ -47,19 +46,19 @@ class PensionSchemeDeclarationReads extends WordSpec with MustMatchers with Opti
        }
      }
      "if we have a dormant field" when {
-       "if we have a company and its dormant field is true" in {
+       "dormant field is true then box 4 is true and box5 is None" in {
          val result = declaration.as[PensionSchemeDeclaration](PensionSchemeDeclaration.apiReads)
          result.box4 mustBe Some(true)
          result.box5 mustBe None
        }
-       "if we have a company and its dormant field is false" in {
+       "dormant field is false then box 5 is true and box4 is None" in {
          val result = (declaration + ("declarationDormant" -> JsBoolean(false))).as[PensionSchemeDeclaration](PensionSchemeDeclaration.apiReads)
          result.box4 mustBe None
          result.box5 mustBe Some(true)
        }
      }
      "if we do not have a dormant field" when {
-       "box 4 and box 5 should be none" in {
+       "box 4 and box 5 should be None" in {
          val result = Json.obj("declaration" -> JsBoolean(true), "declarationDuties" -> JsBoolean(true)).as[PensionSchemeDeclaration](PensionSchemeDeclaration.apiReads)
 
          result.box4 mustBe None
@@ -67,13 +66,14 @@ class PensionSchemeDeclarationReads extends WordSpec with MustMatchers with Opti
        }
      }
      "if we have a duties field" when {
-       "if we have a duties field and is true" in {
+       "It is true, then box10 will be true and box11 is None, addressAndContactDetails is None" in {
          val result = declaration.as[PensionSchemeDeclaration](PensionSchemeDeclaration.apiReads)
          result.box10 mustBe Some(true)
          result.box11 mustBe None
+         result.addressAndContactDetails mustBe None
        }
 
-       "set as false and containing 'adviser' details" in {
+       "It is false and containing 'adviser' details box11 is true, box10 is None and details populated" in {
 
          val advisorDetails = "adviserDetails" -> Json.obj("adviserName" -> JsString("John"),"phoneNumber" -> "07592113", "emailAddress" -> "test@test.com")
          val advisorAddress = "adviserAddress" -> Json.obj("addressLine1" -> JsString("line1"), "addressLine2" -> JsString("line2"), "addressLine3" -> JsString("line3"), "addressLine4" -> JsString("line4"),
@@ -84,12 +84,14 @@ class PensionSchemeDeclarationReads extends WordSpec with MustMatchers with Opti
          val contact=ContactDetails("07592113",None,None,"test@test.com")
          val declaration = Json.obj("declaration" -> JsBoolean(true), "declarationDormant" -> JsBoolean(true), "declarationDuties" -> JsBoolean(false))
          val result = (declaration + advisorDetails + advisorAddress).as[PensionSchemeDeclaration](PensionSchemeDeclaration.apiReads)
+
+         result.box10 mustBe None
          result.box11 mustBe Some(true)
          result.pensionAdviserName mustBe Some(name)
          result.addressAndContactDetails mustBe Some(AddressAndContactDetails(address,contact))
        }
 
-       "if we have a duties field and is false but no contact or name details" in {
+       "if we have a duties field and is false but no contact or name details then addressAndContactDetails be None" in {
          val result = (declaration + ("declarationDuties" -> JsBoolean(false))).as[PensionSchemeDeclaration](PensionSchemeDeclaration.apiReads)
          result.box11 mustBe Some(true)
          result.box10 mustBe None
