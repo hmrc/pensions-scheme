64577e955ccdd5c48386d17d54b950f191bf2284
juan <juanan_rp@hotmail.co.uk>
2018-09-20 09:00:35 +0100

    continuing with scheme details

--- test/models/Reads/schemes/SchemeDetailsReadsSpec.scala
+++ test/models/Reads/schemes/SchemeDetailsReadsSpec.scala
@@ -17,12 +17,20 @@
 package models.Reads.schemes
 
 import org.scalatest.{MustMatchers, OptionValues, WordSpec}
-import play.api.libs.json.{JsString, Json, Reads, Writes}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 class SchemeDetailsReadsSpec extends WordSpec with MustMatchers with OptionValues {
   "A JSON payload containing scheme details" should {
 
-    val schemeDetails = Json.obj("srn" -> JsString("AAABA932JASDA"), "pstr" -> JsString("A3DCADAA"), "schemeStatus" -> "Pending")
+    val schemeDetails = Json.obj("srn" -> JsString("AAABA932JASDA"),
+      "pstr" -> JsString("A3DCADAA"),
+      "schemeStatus" -> "Pending",
+      "schemeName" -> "Test Scheme",
+      "isSchemeMasterTrust" -> JsBoolean(true),
+      "pensionSchemeStructure" -> "Other",
+      "otherPensionSchemeStructure" -> "Other type",
+      "hasMoreThanTenTrustees" -> JsBoolean(true))
 
     "correctly parse to a model of SchemeDetails" when {
       "we have a srn" in {
@@ -32,7 +40,7 @@ class SchemeDetailsReadsSpec extends WordSpec with MustMatchers with OptionValue
       }
 
       "we don't have an srn" in {
-        val output = Json.obj("test" -> "test").as[SchemeDetails]
+        val output =  (schemeDetails - "srn").as[SchemeDetails]
 
         output.srn mustBe None
       }
@@ -44,23 +52,86 @@ class SchemeDetailsReadsSpec extends WordSpec with MustMatchers with OptionValue
       }
 
       "we don't have pstr" in {
-        val output = Json.obj("test" -> "test").as[SchemeDetails]
+        val output = (schemeDetails - "pstr").as[SchemeDetails]
 
-        output.srn mustBe None
+        output.pstr mustBe None
       }
 
       "we have a status" in {
         val output = schemeDetails.as[SchemeDetails]
 
-        output.status mustBe (schemeDetails \ "status").as[String]
+        output.status mustBe (schemeDetails \ "schemeStatus").as[String]
+      }
+
+      "we have a name" in {
+        val output = schemeDetails.as[SchemeDetails]
+
+        output.name mustBe (schemeDetails \ "schemeName").as[String]
+      }
+
+      "we have a flag to say if it is a master trust" in {
+        val output = schemeDetails.as[SchemeDetails]
+
+        output.isMasterTrust mustBe (schemeDetails \ "isSchemeMasterTrust").as[Boolean]
+      }
+
+      "there is no flag to say it is a master trust so we assume it is not" in {
+        val output = (schemeDetails - "isSchemeMasterTrust").as[SchemeDetails]
+
+        output.isMasterTrust mustBe false
+      }
+
+      "we have a type of scheme" in {
+        val output = schemeDetails.as[SchemeDetails]
+
+        output.typeOfScheme.value mustBe (schemeDetails \ "pensionSchemeStructure").as[String]
+      }
+
+      "we don't have a type of scheme" in {
+        val output = (schemeDetails - "pensionSchemeStructure").as[SchemeDetails]
+
+        output.typeOfScheme mustBe None
+      }
+
+      "we have other types of schemes" in {
+        val output = schemeDetails.as[SchemeDetails]
+
+        output.otherTypeOfScheme.value mustBe (schemeDetails \ "otherPensionSchemeStructure").as[String]
+      }
+
+      "we don't have other types of scheme" in {
+        val output = (schemeDetails - "otherPensionSchemeStructure").as[SchemeDetails]
+
+        output.otherTypeOfScheme mustBe None
+      }
+
+      "we have a flag that tells us if there is more than 10 trustees" in {
+        val output = schemeDetails.as[SchemeDetails]
+
+        output.hasMoreThanTenTrustees mustBe (schemeDetails \ "hasMoreThanTenTrustees").as[Boolean]
+      }
+
+      "we don't have a flag that tells us if there is more than 10 trustees so we assume we haven't" in {
+        val output = (schemeDetails - "hasMoreThanTenTrustees").as[SchemeDetails]
+
+        output.hasMoreThanTenTrustees mustBe false
       }
     }
   }
 }
 
-case class SchemeDetails(srn: Option[String], pstr: Option[String], status: String)
+case class SchemeDetails(srn: Option[String], pstr: Option[String], status: String, name: String, isMasterTrust: Boolean, typeOfScheme: Option[String], otherTypeOfScheme: Option[String], hasMoreThanTenTrustees: Boolean)
 
 object SchemeDetails {
-  implicit val reads : Reads[SchemeDetails] = Json.reads[SchemeDetails]
+  implicit val reads : Reads[SchemeDetails] = (
+    (JsPath \ "srn").readNullable[String] and
+      (JsPath \ "pstr").readNullable[String] and
+    (JsPath \ "schemeStatus").read[String] and
+      (JsPath \ "schemeName").read[String] and
+      (JsPath \ "isSchemeMasterTrust").readNullable[Boolean] and
+      (JsPath \ "pensionSchemeStructure").readNullable[String] and
+      (JsPath \ "otherPensionSchemeStructure").readNullable[String] and
+      (JsPath \ "hasMoreThanTenTrustees").readNullable[Boolean]
+  )((srn,pstr,status,name,isMasterTrust,typeOfScheme,otherTypeOfScheme,moreThan10Trustees) => SchemeDetails(srn,pstr,status,name,isMasterTrust.getOrElse(false),typeOfScheme,otherTypeOfScheme,moreThan10Trustees.getOrElse(false)))
   implicit val writes : Writes[SchemeDetails] = Json.writes[SchemeDetails]
 }
