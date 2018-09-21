799be54b4065a5b1ea4ae3d684ab2b754689d4cc
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-09-21 10:55:20 +0100

    Remove comments

--- app/repositories/PensionsSchemeCacheRepository.scala
+++ app/repositories/PensionsSchemeCacheRepository.scala
@@ -116,7 +116,6 @@ abstract class PensionsSchemeCacheRepository(
         val unencrypted = PlainText(Json.stringify(data))
         val encryptedData = jsonCrypto.encrypt(unencrypted).value
         val dataAsByteArray: Array[Byte] = encryptedData.getBytes("UTF-8")
-        //val dataAsByteArray = StandardCharsets.UTF_8.encode(encryptedData).array
         Json.toJson(DataEntry(id, dataAsByteArray))
       } else
         Json.toJson(JsonDataEntry(id, data, DateTime.now(DateTimeZone.UTC)))
