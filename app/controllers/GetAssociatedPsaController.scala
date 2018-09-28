149c9932e7c23cf8fc80c8f05f94447d679fa904
Jack Reed <jack.reed@digital.hmrc.gov.uk>
2018-09-28 13:29:12 +0100

    Created controller, tests, amended expected data WIP

---
+++ app/controllers/GetAssociatedPsaController.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+
+import com.google.inject.Inject
+import connector.SchemeConnector
+import play.api.Logger
+import play.api.mvc._
+import uk.gov.hmrc.http._
+import uk.gov.hmrc.play.bootstrap.controller.BaseController
+import utils.ErrorHandler
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+
+class GetAssociatedPsaController @Inject()(schemeConnector: SchemeConnector) extends BaseController with ErrorHandler {
+
+  def getSchemeDetails: Action[AnyContent] = Action.async {
+    implicit request => {
+      val psaId = request.headers.get("psaId")
+      val srn = request.headers.get("schemeReferenceNumber")
+
+      srn match {
+        case Some(schemeReferenceNumber) =>
+          schemeConnector.getSchemeDetails("srn", schemeReferenceNumber).map {
+            case Right(schemeDetails) => Ok(schemeDetails)
+            case Left(e)
+            => result(e)
+          }
+        case _ => Future.failed(new BadRequestException("Bad Request with missing parameters PSA Id or SRN"))
+      }
+    } recoverWith recoverFromError
+  }
+}
---
+++ test/controllers/GetAssociatedPsaControllerSpec.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.JsonFileReader
+import connector.SchemeConnector
+import org.mockito.Matchers
+import org.mockito.Matchers.any
+import org.mockito.Mockito.{reset, when}
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{AsyncWordSpec, BeforeAndAfter, MustMatchers, RecoverMethods}
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.BadRequestException
+
+import scala.concurrent.Future
+
+class GetAssociatedPsaControllerSpec extends AsyncWordSpec with MockitoSugar with BeforeAndAfter with MustMatchers with JsonFileReader with RecoverMethods {
+
+  val mockSchemeConnector: SchemeConnector = mock[SchemeConnector]
+  val associatedPsaController = new GetAssociatedPsaController(mockSchemeConnector)
+  private val schemeReferenceNumber = "S999999999"
+  private val psaIdNumber = "A1234567"
+
+  def errorResponse(code: String): String = {
+    Json.stringify(
+      Json.obj(
+        "code" -> code,
+        "reason" -> s"Reason for $code"
+      )
+    )
+  }
+
+  before {
+    reset(mockSchemeConnector)
+  }
+
+  "getAssociatedPsa" must {
+
+    def fakeRequest: FakeRequest[AnyContentAsEmpty.type] =
+      FakeRequest("GET", "/").withHeaders(("psaId", psaIdNumber), ("schemeReferenceNumber", schemeReferenceNumber))
+
+    "return OK when the scheme is registered successfully" in {
+
+      val successResponse: JsValue = readJsonFromFile("/data/validAssociatedPsaResponse.json")
+
+      when(mockSchemeConnector.getSchemeDetails(Matchers.eq("srn"), Matchers.eq(schemeReferenceNumber))(any(), any(), any())).thenReturn(
+        Future.successful(Right(successResponse)))
+
+      associatedPsaController.getSchemeDetails()(fakeRequest) map { result =>
+        result.header.status mustBe OK
+        result.body mustBe successResponse
+      }
+    }
+  }
+
+  "throw BadRequestException when the Scheme Reference Number is not present in the header" in {
+
+//    e.getMessage mustBe "Bad Request with missing parameters PSA Id or SRN"
+//    verify(mockSchemeConnector, never()).getSchemeDetails(Matchers.any(),
+//      Matchers.any())(any(), any(), any())
+
+    recoverToSucceededIf[BadRequestException] {
+      associatedPsaController.getSchemeDetails()(FakeRequest("GET", "/").withHeaders(("psaIdNumber", psaIdNumber)))
+    }
+
+  }
+}
