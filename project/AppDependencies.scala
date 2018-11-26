0cc3377049d07377474e62d5ee112737c52789d4
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2018-11-26 17:24:59 +0000

    PODS-1905-Added build dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,11 +18,10 @@ import sbt._
 import play.sbt.PlayImport.{ws, _}
 import play.core.PlayVersion
 
-object MicroServiceBuild extends Build with MicroService {
+object AppDependencies {
 
   val appName = "pensions-scheme"
 
-  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
   private val scalaTestPlusPlayVersion = "3.1.2"
   private val mockitoAllVersion = "1.10.19"
   private val wireMockVersion = "2.19.0"
