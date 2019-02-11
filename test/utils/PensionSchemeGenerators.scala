fcbafd1fb598c7d94863c75a6f2c6ed8d6ccc0cf
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2019-02-11 18:43:47 +0000

    PODS-2297-Added address transformer

--- test/utils/PensionSchemeGenerators.scala
+++ test/utils/PensionSchemeGenerators.scala
@@ -18,13 +18,15 @@ package utils
 
 import models.{Address, InternationalAddress, UkAddress}
 import org.scalacheck.Gen
+import play.api.libs.json.{JsValue, Json}
 
 trait PensionSchemeGenerators {
   val schemaValidator = SchemaValidatorForTests()
-  val addressLineGen : Gen[String] = Gen.listOfN[Char] (35, Gen.alphaChar).map (_.mkString)
+  val addressLineGen: Gen[String] = Gen.listOfN[Char](35, Gen.alphaChar).map(_.mkString)
   val addressLineOptional: Gen[Option[String]] = Gen.option(addressLineGen)
-  val postalCodeGem: Gen[String] = Gen.listOfN[Char] (10, Gen.alphaChar).map (_.mkString)
-  val countryCode: Gen[String] = Gen.oneOf(Seq("ES","IT"))
+  val postalCodeGem: Gen[String] = Gen.listOfN[Char](10, Gen.alphaChar).map(_.mkString)
+  val optionalPostalCodeGen: Gen[Option[String]] = Gen.option(Gen.listOfN[Char](10, Gen.alphaChar).map(_.mkString))
+  val countryCode: Gen[String] = Gen.oneOf(Seq("ES", "IT"))
 
   val ukAddressGen: Gen[Address] = for {
     line1 <- addressLineGen
@@ -32,7 +34,7 @@ trait PensionSchemeGenerators {
     line3 <- addressLineOptional
     line4 <- addressLineOptional
     postalCode <- postalCodeGem
-  } yield UkAddress(line1,Some(line2),line3,line4,"GB",postalCode)
+  } yield UkAddress(line1, Some(line2), line3, line4, "GB", postalCode)
 
   val internationalAddressGen: Gen[Address] = for {
     line1 <- addressLineGen
@@ -40,5 +42,40 @@ trait PensionSchemeGenerators {
     line3 <- addressLineOptional
     line4 <- addressLineOptional
     countryCode <- countryCode
-  } yield InternationalAddress(line1,Some(line2),line3,line4,countryCode)
+  } yield InternationalAddress(line1, Some(line2), line3, line4, countryCode)
+
+  def addressJsValueGen(isDifferent: Boolean = false): Gen[(JsValue, JsValue)] = for {
+    line1 <- addressLineGen
+    line2 <- addressLineGen
+    line3 <- addressLineOptional
+    line4 <- addressLineOptional
+    postalCode <- optionalPostalCodeGen
+    countryCode <- countryCode
+  } yield {
+    (
+      Json.obj(
+        "desAddress" -> (Json.obj("nonUKAddress" -> true) ++
+          Json.obj("line1" -> line1) ++
+          Json.obj("line2" -> line2) ++
+          optional("line3", line3) ++
+          optional("line4", line4) ++
+          optional("postalCode", postalCode) ++
+          Json.obj("countryCode" -> countryCode))
+      ),
+      Json.obj(
+        "userAnswersAddress" -> (Json.obj("addressLine1" -> line1) ++
+          Json.obj("addressLine2" -> line2) ++
+          optional("addressLine3", line3) ++
+          optional("addressLine4", line4) ++
+          optional(if (isDifferent) "postcode" else "postalCode", postalCode) ++
+          Json.obj((if (isDifferent) "country" else "countryCode") -> countryCode))
+      )
+    )
+  }
+
+  private def optional(key: String, element: Option[String]) = {
+    element.map { value =>
+      Json.obj(key -> value)
+    }.getOrElse(Json.obj())
+  }
 }
