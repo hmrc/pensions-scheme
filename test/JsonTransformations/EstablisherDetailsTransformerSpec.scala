d7041b85e5741de3249014142d35245d789b8de8
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-02-14 11:42:03 +0000

    PODS-2278 Split tests into two files

--- test/JsonTransformations/EstablisherDetailsTransformerSpec.scala
+++ test/JsonTransformations/EstablisherDetailsTransformerSpec.scala
@@ -23,67 +23,84 @@ import utils.JsonTransformations.EstablisherDetailsTransformer
 
 
 class EstablisherDetailsTransformerSpec extends WordSpec with MustMatchers with OptionValues with JsonFileReader {
-  private val desSchemeDetailsJsValue: JsValue = readJsonFromFile("/data/validGetSchemeDetails.json")
+  private val desSchemeDetailsJsValue1: JsValue = readJsonFromFile("/data/validGetSchemeDetails1.json")
+  private val desSchemeDetailsJsValue2: JsValue = readJsonFromFile("/data/validGetSchemeDetails2.json")
 
   private val desPersonDetailsArrayElementReads: Reads[JsArray] = (__ \ 'psaSchemeDetails \ 'establisherDetails \ 'individualDetails).json.pick[JsArray]
- // private val desPersonDetailsElementReads: Reads[JsValue] = (__ \ 'personDetails).json.pick
+  // private val desPersonDetailsElementReads: Reads[JsValue] = (__ \ 'personDetails).json.pick
   private val IndividualDetailsElementReads: Reads[JsValue] = (__).json.pick
 
   private val transformer = new EstablisherDetailsTransformer()
 
-  private val desIndividualDetailsSeqJsValue = desSchemeDetailsJsValue.transform(desPersonDetailsArrayElementReads).asOpt.get.value
+  private def desIndividualDetailsSeqJsValue(jsValue: JsValue) = jsValue.transform(desPersonDetailsArrayElementReads).asOpt.get.value
 
   "A DES payload containing establisher details" must {
-    "Map correctly to valid establisher details in user answers" when {
-      desIndividualDetailsSeqJsValue.zipWithIndex.foreach {
+    "Map correctly to valid establisher details in user answers for first json file" when {
+      desIndividualDetailsSeqJsValue(desSchemeDetailsJsValue1).zipWithIndex.foreach {
         case (desIndividualDetailsJsValue, index) =>
-        val desIndividualDetailsElementJsValue = desIndividualDetailsJsValue.transform(IndividualDetailsElementReads).asOpt.get
-        s"we have establisher details for element $index in establishers array" in {
+          val desIndividualDetailsElementJsValue = desIndividualDetailsJsValue.transform(IndividualDetailsElementReads).asOpt.get
+          s"we have establisher details for element $index in establishers array" in {
 
-          val actual = desIndividualDetailsElementJsValue
-            .transform(transformer.transformPersonDetailsToUserAnswersReads).asOpt.get
+            val actual = desIndividualDetailsElementJsValue
+              .transform(transformer.transformPersonDetailsToUserAnswersReads).asOpt.get
 
 
-          (actual \ "establisherDetails" \ "firstName").as[String] mustBe (desIndividualDetailsJsValue \ "personDetails" \ "firstName").as[String]
-          (actual \ "establisherDetails" \ "middleName").asOpt[String] mustBe (desIndividualDetailsJsValue \ "personDetails" \ "middleName").asOpt[String]
-          (actual \ "establisherDetails" \ "lastName").as[String] mustBe (desIndividualDetailsJsValue \ "personDetails" \ "lastName").as[String]
-          (actual \ "establisherDetails" \ "date").as[String] mustBe (desIndividualDetailsJsValue \ "personDetails" \ "dateOfBirth").as[String]
-        }
+            (actual \ "establisherDetails" \ "firstName").as[String] mustBe (desIndividualDetailsJsValue \ "personDetails" \ "firstName").as[String]
+            (actual \ "establisherDetails" \ "middleName").asOpt[String] mustBe (desIndividualDetailsJsValue \ "personDetails" \ "middleName").asOpt[String]
+            (actual \ "establisherDetails" \ "lastName").as[String] mustBe (desIndividualDetailsJsValue \ "personDetails" \ "lastName").as[String]
+            (actual \ "establisherDetails" \ "date").as[String] mustBe (desIndividualDetailsJsValue \ "personDetails" \ "dateOfBirth").as[String]
+          }
 
-        s"we have nino details for element $index in establishers array" in {
+          s"we have nino details for element $index in establishers array" in {
 
-          val actual = desIndividualDetailsElementJsValue
-            .transform(transformer.transformNinoDetailsToUserAnswersReads).asOpt.get
-
-          if ((desIndividualDetailsJsValue \ "nino").isDefined) {
+            val actual = desIndividualDetailsElementJsValue
+              .transform(transformer.transformNinoDetailsToUserAnswersReads).asOpt.get
 
             (actual \ "establisherNino" \ "hasNino").as[Boolean] mustBe true
             (actual \ "establisherNino" \ "nino").asOpt[String] mustBe (desIndividualDetailsJsValue \ "nino").asOpt[String]
-
-          } else {
-            (actual \ "establisherNino" \ "hasNino").as[Boolean] mustBe false
-            (actual \ "establisherNino" \ "reason").asOpt[String] mustBe (desIndividualDetailsJsValue \ "noNinoReason").asOpt[String]
           }
-        }
 
           s"we have utr details for element $index in establishers array" in {
 
             val actual = desIndividualDetailsElementJsValue
               .transform(transformer.transformUtrDetailsToUserAnswersReads).asOpt.get
 
-            if ((desIndividualDetailsJsValue \ "utr").isDefined) {
+            (actual \ "uniqueTaxReference" \ "hasUtr").as[Boolean] mustBe true
+            (actual \ "uniqueTaxReference" \ "utr").asOpt[String] mustBe (desIndividualDetailsJsValue \ "utr").asOpt[String]
 
-              (actual \ "uniqueTaxReference" \ "hasUtr").as[Boolean] mustBe true
-              (actual \ "uniqueTaxReference" \ "utr").asOpt[String] mustBe (desIndividualDetailsJsValue \ "utr").asOpt[String]
-
-            } else {
-              (actual \ "uniqueTaxReference" \ "hasUtr").as[Boolean] mustBe false
-              (actual \ "uniqueTaxReference" \ "reason").asOpt[String] mustBe (desIndividualDetailsJsValue \ "noUtrReason").asOpt[String]
-            }
           }
 
       }
 
     }
+
+
+    "Map correctly to valid establisher details in user answers for second json file" when {
+      desIndividualDetailsSeqJsValue(desSchemeDetailsJsValue2).zipWithIndex.foreach {
+        case (desIndividualDetailsJsValue, index) =>
+          val desIndividualDetailsElementJsValue = desIndividualDetailsJsValue.transform(IndividualDetailsElementReads).asOpt.get
+
+          s"we have nino details for element $index in establishers array" in {
+
+            val actual = desIndividualDetailsElementJsValue
+              .transform(transformer.transformNinoDetailsToUserAnswersReads).asOpt.get
+
+            (actual \ "establisherNino" \ "hasNino").as[Boolean] mustBe false
+            (actual \ "establisherNino" \ "reason").asOpt[String] mustBe (desIndividualDetailsJsValue \ "noNinoReason").asOpt[String]
+          }
+
+          s"we have utr details for element $index in establishers array" in {
+
+            val actual = desIndividualDetailsElementJsValue
+              .transform(transformer.transformUtrDetailsToUserAnswersReads).asOpt.get
+
+            (actual \ "uniqueTaxReference" \ "hasUtr").as[Boolean] mustBe false
+            (actual \ "uniqueTaxReference" \ "reason").asOpt[String] mustBe (desIndividualDetailsJsValue \ "noUtrReason").asOpt[String]
+          }
+
+      }
+
+    }
+
   }
 }
