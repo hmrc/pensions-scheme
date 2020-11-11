77cb966cda6037a213a60159444c72b447cb1b17
ashwani1403 <ashwani.kumar@digital.hmrc.gov.uk>
2020-11-11 20:35:05 +0000

    PODS-4886: if test wip

---
+++ test/models/etmpToUseranswers/DES/TransformationSpec.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.etmpToUseranswers.DES
+
+import base.JsonFileReader
+import org.scalatest.MustMatchers
+import org.scalatest.OptionValues
+import org.scalatest.WordSpec
+import play.api.inject.Injector
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.Configuration
+import play.api.Environment
+import utils.PensionSchemeJsValueGenerators
+
+trait TransformationSpec extends WordSpec with MustMatchers with OptionValues with JsonFileReader with PensionSchemeJsValueGenerators {
+
+  val injector: Injector = new GuiceApplicationBuilder().build().injector
+  val config: Configuration = injector.instanceOf[Configuration]
+  val environment: Environment = injector.instanceOf[Environment]
+}
