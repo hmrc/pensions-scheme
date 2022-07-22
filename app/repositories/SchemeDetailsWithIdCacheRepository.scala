e27834ae7f164506c66885cd584e29b859b04f23
Mary1-5 <marymansha15@gmail.com>
2022-07-22 10:05:00 +0100

    WIP

--- app/repositories/SchemeDetailsWithIdCacheRepository.scala
+++ app/repositories/SchemeDetailsWithIdCacheRepository.scala
@@ -19,76 +19,113 @@ package repositories
 import com.google.inject.Inject
 import models.SchemeWithId
 import org.joda.time.{DateTime, DateTimeZone}
-import org.slf4j.{Logger, LoggerFactory}
-import play.api.Configuration
+import org.mongodb.scala.bson.BsonBinary
+import org.mongodb.scala.model._
 import play.api.libs.json._
-import play.modules.reactivemongo.ReactiveMongoComponent
-import reactivemongo.api.indexes.Index
-import reactivemongo.api.indexes.IndexType.Ascending
-import reactivemongo.bson.{BSONDocument, BSONObjectID}
-import reactivemongo.play.json.ImplicitBSONHandlers._
-import uk.gov.hmrc.mongo.ReactiveRepository
-import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
+import play.api.{Configuration, Logging}
+import repositories.SchemeCacheRepository.JsonDataEntry
+import repositories.SchemeDetailsWithIdCacheRepository.SchemeDetailsWithIdCacheRepositoryFormats.{dataKey, expireAtKey, idField, lastUpdatedKey}
+import repositories.SchemeDetailsWithIdCacheRepository.{DataEntry, JsonDataEntry, SchemeDetailsWithIdCacheRepositoryFormats, SchemeDetailsWithIdDataEntry}
+import uk.gov.hmrc.mongo.MongoComponent
+import uk.gov.hmrc.mongo.play.json.formats.MongoBinaryFormats.{byteArrayReads, byteArrayWrites}
+import uk.gov.hmrc.mongo.play.json.formats.MongoJodaFormats
+import uk.gov.hmrc.mongo.play.json.{Codecs, PlayMongoRepository}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class SchemeDetailsWithIdCacheRepository @Inject()(
-                                     mongoComponent: ReactiveMongoComponent,
-                                     configuration: Configuration
-                                   )(implicit val ec: ExecutionContext)
-  extends ReactiveRepository[JsValue, BSONObjectID](
-    configuration.get[String](path = "mongodb.pensions-scheme-cache.scheme-with-id.name"),
-    mongoComponent.mongoConnector.db,
-    implicitly
-  ) {
+object SchemeDetailsWithIdCacheRepository {
 
-  override val logger: Logger = LoggerFactory.getLogger("SchemeDetailsWithIdCacheRepository")
+//  sealed trait SchemeDetailsWithIdDataEntry
+
+//  case class JsonDataEntry(id: String, data: JsValue, lastUpdated: DateTime, expireAt: DateTime) extends SchemeDetailsWithIdDataEntry
+
+
+  object JsonDataEntry {
+    implicit val dateFormat: Format[DateTime] = MongoJodaFormats.dateTimeFormat
+    implicit val format: Format[JsonDataEntry] = Json.format[JsonDataEntry]
+  }
+
+  object SchemeDetailsWithIdCacheRepositoryFormats {
+    implicit val dateFormat: Format[DateTime] = MongoJodaFormats.dateTimeFormat
+    implicit val format: Format[SchemeDetailsWithIdDataEntry] = Json.format[SchemeDetailsWithIdDataEntry]
+
+    val dataKey: String = "data"
+    val idField: String = "id"
+    val lastUpdatedKey: String = "lastUpdated"
+    val expireAtKey: String = "expireAt"
+  }
+}
+
+class SchemeDetailsWithIdCacheRepository @Inject()(
+                                                    mongoComponent: MongoComponent,
+                                                    configuration: Configuration
+                                   )(implicit val ec: ExecutionContext)
+  extends PlayMongoRepository[SchemeDetailsWithIdDataEntry](
+    mongoComponent = mongoComponent,
+    collectionName = configuration.get[String](path = "mongodb.pensions-scheme-cache.scheme-with-id.name"),
+    domainFormat = SchemeDetailsWithIdCacheRepositoryFormats.format,
+    extraCodecs = Seq(
+      Codecs.playFormatCodec(JsonDataEntry.format),
+      Codecs.playFormatCodec(DataEntry.format)
+    ),
+    indexes = Seq(
+      IndexModel(
+        Indexes.ascending(lastUpdatedKey),
+        IndexOptions().name(expireAtKey).background(true)
+      )
+    )
+  ) with Logging {
+
+  private val filterScheme = Filters.equal("uniqueSchemeWithId", _: String)
+
 
   private def expireInSeconds: DateTime = DateTime.now(DateTimeZone.UTC).
     plusSeconds(configuration.get[Int](path = "mongodb.pensions-scheme-cache.scheme-with-id.timeToLiveInSeconds"))
 
-  override lazy val indexes: Seq[Index] = Seq(
-    Index(key = Seq("uniqueSchemeWithId" -> Ascending), name = Some("schemeId_userId_index"), unique = true),
-    Index(key = Seq("expireAt" -> Ascending), name = Some("dataExpiry"), options = BSONDocument("expireAfterSeconds" -> 0))
-  )
+//  override lazy val indexes: Seq[Index] = Seq(
+//    Index(key = Seq("uniqueSchemeWithId" -> Ascending), name = Some("schemeId_userId_index"), unique = true),
+//    Index(key = Seq("expireAt" -> Ascending), name = Some("dataExpiry"), options = BSONDocument("expireAfterSeconds" -> 0))
+//  )
 
-  (for { _ <- ensureIndexes } yield { () }) recoverWith {
-    case t: Throwable => Future.successful(logger.error(s"Error creating indexes on collection ${collection.name}", t))
-  } andThen {
-    case _ => CollectionDiagnostics.logCollectionInfo(collection)
-  }
-
-  override def ensureIndexes(implicit ec: ExecutionContext): Future[Seq[Boolean]] =
-    Future.sequence(indexes.map(collection.indexesManager.ensure(_)))
-
-  private val selector: SchemeWithId => BSONDocument = schemeWithId =>
-    BSONDocument("uniqueSchemeWithId" -> (schemeWithId.schemeId + schemeWithId.userId))
-
-  private val modifier: JsValue => BSONDocument = document =>
-    BSONDocument("$set" -> document)
+//  private val selector: SchemeWithId => BSONDocument = schemeWithId =>
+//    BSONDocument("uniqueSchemeWithId" -> (schemeWithId.schemeId + schemeWithId.userId))
+//
+//  private val modifier: JsValue => BSONDocument = document =>
+//    BSONDocument("$set" -> document)
 
   def save(schemeWithId: SchemeWithId, schemeDetails: JsValue): Future[Boolean] = {
+    val id: String = schemeWithId.schemeId + schemeWithId.userId
+
+    val modifier = Updates.combine(
+      Updates.set(idField, id),
+      Updates.set(dataKey, Codecs.toBson(schemeDetails)),
+      Updates.set(lastUpdatedKey, Codecs.toBson(DateTime.now(DateTimeZone.UTC)))
+    )
+    collection.findOneAndUpdate(filterScheme(id), modifier).toFuture().map(_ => true)
+
+    /*
+    def save(schemeWithId: SchemeWithId, schemeDetails: JsValue): Future[Boolean] = {
     val id: String = schemeWithId.schemeId + schemeWithId.userId
     val document: JsValue = Json.toJson(DataCache.applyDataCache(id, schemeDetails, expireAt = expireInSeconds))
     collection.update(true).one(selector(schemeWithId), modifier(document), upsert = true).map(_.ok)
   }
+     */
+  }
 
-  def get(schemeWithId: SchemeWithId): Future[Option[JsValue]] =
-    collection.find(selector(schemeWithId), Option.empty[JsObject]).one[DataCache].map(_.map(_.data))
+  def get(schemeWithId: SchemeWithId): Future[Option[JsValue]] = {
+    collection.find[JsonDataEntry](Filters.equal(idField, schemeWithId)).headOption().map{
+      _.map {
+        dataEntry =>
+          dataEntry.data
+      }
+    }
+  }
 
-  def remove(schemeWithId: SchemeWithId): Future[Boolean] =
-    collection.delete.one(selector(schemeWithId)).map(_.ok)
+  def remove(schemeWithId: SchemeWithId): Future[Boolean] = {
+    collection.deleteOne(Filters.equal(idField,SchemeWithId)).toFuture().map { result =>
+      logger.info(s"Removing row from collection $collectionName externalId:${schemeWithId.schemeId}")
+      result.wasAcknowledged
+    }
+  }
 
-}
-
-private case class DataCache(id: String, data: JsValue, lastUpdated: DateTime, expireAt: DateTime)
-
-private object DataCache {
-  implicit val dateFormat: Format[DateTime] = ReactiveMongoFormats.dateTimeFormats
-  implicit val format: Format[DataCache] = Json.format[DataCache]
-
-  def applyDataCache(id: String, data: JsValue,
-            lastUpdated: DateTime = DateTime.now(DateTimeZone.UTC),
-            expireAt: DateTime): DataCache =
-    DataCache(id, data, lastUpdated, expireAt)
 }
